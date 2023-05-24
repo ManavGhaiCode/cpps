7111d53c7128109e04d7d1441e2491bcf3997f7b
ManavGhaiCode <manavghai@gmail.com>
2023-05-25 00:14:17 +0530

    add include protection to PCHs

--- PCH/iostr.h
+++ PCH/iostr.h
@@ -1,1 +1,6 @@
+#ifndef _IOSTR_H_
+#define _IOSTR_H_
+
 #include <iostream>
+
+#endif
--- PCH/std.h
+++ PCH/std.h
@@ -1,3 +1,8 @@
+#ifndef _STD_H_
+#define _STD_H_
+
 #include <iostream>
 #include <vector>
 #include <string>
+
+#endif
