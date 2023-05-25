f067b0ccd727af39c376469e6e30b7d6a9432a1a
ManavGhaiCode <manavghai@gmail.com>
2023-05-25 14:06:41 +0530

    add function tax();

--- math/math.h
+++ math/math.h
@@ -12,6 +12,31 @@ namespace Math {
     template<typename T> T ceil(T num) {
         return std::ceil(num);
     }
+
+        template<typename T> double tax(T numToTax) {
+        int b1n1 = 250000;
+        int b1n2 = 500000;
+        int b2n2 = 750000;
+        int b3n2 = 1000000;
+        int b4n2 = 1250000;
+        int b5n2 = 1500000;
+
+        if (numToTax > b1n1) {
+            return numToTax;
+        } else if (numToTax > b1n1 and numToTax <= b1n2) {
+            return numToTax - (numToTax * (5 / 100));
+        } else if (numToTax > b1n2 and numToTax <= b2n2) {
+            return numToTax - (numToTax * (10 / 100));
+        } else if (numToTax > b2n2 and numToTax <= b3n2) {
+            return numToTax - (numToTax * (15 / 100));
+        } else if (numToTax > b3n2 and numToTax <= b4n2) {
+            return numToTax - (numToTax * (20 / 100));
+        } else if (numToTax > b1n2 and numToTax <= b5n2) {
+            return numToTax - (numToTax * (25 / 100));
+        } else {
+            return numToTax - (numToTax * (30 / 100));
+        }
+    }
 }
 
 #endif
