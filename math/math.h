3b3adf1bfac94ff41fde96e71a84dd798156d4bf
ManavGhaiCode <manavghai@gmail.com>
2023-05-25 14:05:51 +0530

    add function floor(); and function ceil();

--- math/math.h
+++ math/math.h
@@ -1,8 +1,17 @@
 #ifndef _MATH_H_
 #define _MATH_H_
 
-namespace Math {
+#include "../PCH/std.h"
+#include <cmath>
 
+namespace Math {
+    template<typename T> T floor(T num) {
+        return std::floor(num);
+    }
+
+    template<typename T> T ceil(T num) {
+        return std::ceil(num);
+    }
 }
 
 #endif
